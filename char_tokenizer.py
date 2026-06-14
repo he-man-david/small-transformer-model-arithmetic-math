5fde70b6fb99ee794959c6000e6c87619cc8da40
David XYZ <davidhe88@pm.me>
2026-06-14 01:37:31 +0000

    training run finished but need to increase model size

--- char_tokenizer.py
+++ char_tokenizer.py
@@ -3,7 +3,7 @@ import torch
 class TinyArithmeticCharTokenizer:
     def __init__(self):
         self.all_chars = "0123456789+-*/=."
-        self.special_tokens = [" ", "<pad>", "<eos>", "<unk>"]
+        self.special_tokens = [" ", "<pad>", "<unk>", "<eos>"]
         self.vocab_list = list(self.all_chars) + self.special_tokens
         
         self.char_to_int = {char: i for i, char in enumerate(self.vocab_list)}
@@ -11,17 +11,15 @@ class TinyArithmeticCharTokenizer:
         self.vocab_size = len(self.vocab_list)
 
     def encode(self, input_string: str) -> list[int]:
-        """Converts raw text into a list of integer token IDs."""
         return [self.char_to_int.get(char, self.char_to_int["<unk>"]) for char in input_string]
 
     def decode(self, token_ids: list[int]) -> str:
-        """Converts a list of integer token IDs back into readable text."""
         decoded_chars = []
         for token_id in token_ids:
             char = self.int_to_char.get(token_id)
-            if char is not None and char not in ["<eos>", "<pad>", "<unk>", " "]:
+            if char == "<eos>":
+                break
+            if char is not None and char not in ["<pad>", "<unk>"]:
                 decoded_chars.append(char)
-            elif char == "<eos>":
-                break  # Stop decoding once we hit the end of the text generation
         return "".join(decoded_chars)
 
